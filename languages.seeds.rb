b9a24845c52bd4e9916c344b1c362c217cc30459
cromon <e@cromon.co.uk>
2018-11-08 20:44:24 +0000

    Added a few

--- languages.seeds.rb
+++ languages.seeds.rb
@@ -6,10 +6,10 @@
 # Country { name: 'String', native: 'String', family: 'String', scope: 'String', type_of: 'String', iso_639_1: 'String', iso_639_2b: 'String', iso_639_3: 'String' }
 #
 [
-  { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
-  { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
-  { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
-  { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
+  { name: 'Ghotuo', native: '', family: 'Niger-Congo', scope: 'I', type_of: 'L', iso_639_1: '', iso_639_2b: '', iso_639_3: 'aaa' },
+  { name: 'Alumu-Tesu	', native: '', family: 'Niger-Congo', scope: 'I', type_of: 'L', iso_639_1: '', iso_639_2b: '', iso_639_3: 'aab' },
+  { name: 'Ari', native: '', family: 'Trans-New Guinea', scope: 'I', type_of: 'L', iso_639_1: '', iso_639_2b: '', iso_639_3: 'aac' },
+  { name: 'Amal', native: '', family: 'Sepik', scope: 'I', type_of: 'L', iso_639_1: '', iso_639_2b: '', iso_639_3: 'aad' },
   { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
   { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
   { name: '', native: '', family: '', scope: '', type_of: '', iso_639_1: '', iso_639_2b: '', iso_639_3: '' },
